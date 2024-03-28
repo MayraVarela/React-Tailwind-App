29dc0d3ae650d5079e6d06330527ae431170cd35
MayraVarela <Mayravarelaparra16@gmail.com>
2024-03-28 15:52:11 +0100

    pagina terminada

--- React-Tailwind/src/App.jsx
+++ React-Tailwind/src/App.jsx
@@ -1,17 +1,18 @@
 import { Head } from "./componentes/Head"
 import { MainArticle } from "./componentes/MainArticle"
 import { NewContainer } from "./componentes/NewContainer"
-
+import { ArticlesContainer } from "./componentes/ArticlesContainer"
 function App() {
 
 
   return (
     <main className= "px-4 pt-6">
     <Head />
-    <div className="sm:flex sm:gap-8">
+    <div className="lg:flex lg:gap-8">
     <MainArticle />
     <NewContainer />
     </div>
+    <ArticlesContainer />
     </main>
   ) 
 }
